c664b2008aa2eb94b739e63358fe9baa8170bbc6
ZhangJian He <shoothzj@gmail.com>
2024-12-26 14:58:06 +0800

    feat: add MustGetInt method (#8)
    Signed-off-by: ZhangJian He <shoothzj@gmail.com>

--- envx_str.go
+++ envx_str.go
@@ -9,3 +9,12 @@ func GetStrOr(key string, defaultValue string) string {
 	}
 	return env
 }
+
+// MustGet returns the string value of an environment variable, panicking if it's missing.
+func MustGet(key string) string {
+	aux := os.Getenv(key)
+	if aux == "" {
+		panic(NewMissingEnvVarError(key))
+	}
+	return aux
+}
